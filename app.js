7b65a526784627c3b9447e17b3a91cb39ace0bf6
Anatolie Diordita <contact@anatolie.me>
2017-10-24 12:13:44 -0400

    Changed API to use ExpressJS for routing.

--- app.js
+++ app.js
@@ -1,82 +1,59 @@
 /**
- * Dead simple HTTP server to simulate an API used for testing
- * frontend of Spectero VueJS application. Filled w/ fake data.
- * 
- * Made in 10 minutes so no judgment on the quality of the code ^_^
+ * Simple ExpressJS app to simulate API used for Spectero frontend.
  */
 
-const http = require('http')
-const port = 3000
+const express = require('express')
+const app = express()
+const router = express.Router()
 
-const data = require('./dummy-data.js')
+const data = require('./data.js')
 
-const requestHandler = (request, response) => {
-  
-  let result = {
-    code: 200,
-    errors: [],
-    message: null,
-    data: null,
-    endpoint: null
+const port = 3000 // Port for server (default: 3000)
+
+let formatResponse = function (code, errors, message, data, endpoint) {
+  return {
+    code: code,
+    errors: errors,
+    message: message,
+    data: data,
+    endpoint: endpoint
   }
-
-  let routes = {
-    '/nodes': {
-      GET: function () {
-        return {
-          nodes: data.nodes()
-        }
-      }
-    }
-  }
-
-  let errorNotFound = function (result) {
-    result.code = 404
-    result.errors.push(`404 Not Found`)
-    response.statusCode = 404
-    return response.end(JSON.stringify(result))
-  }
-
-  let errorMethodNotAllowed = function (result) {
-    result.code = 405
-    result.errors.push(`404 Method Not Allowed`)
-    response.statusCode = 405
-    return response.end(JSON.stringify(result))
-  }
-
-  console.log(`${request.method}  ${request.url}`)
-
-  // Check if URL has route associated to it
-  if ( routes[request.url] === undefined ) {
-    return errorNotFound(result)
-  }
-
-  // Instead of having to declare GET method, can just directly feed
-  // a function into the route if only GET method req'd.
-  if ( typeof routes[request.url] === 'function' ) {
-    result.data = routes[request.url]()
-  }
-
-  // Check if route method exists
-  else if ( routes[request.url][request.method] === undefined ) {
-    return errorMethodNotAllowed(result)
-  }
-
-  else {
-    result.data = routes[request.url][request.method]()
-  }
-
-  response.setHeader(`Content-Type`, `application/json`)
-  response.setHeader(`Access-Control-Allow-Origin`, `*`)
-  response.end(JSON.stringify(result))
-  
 }
 
-const server = http.createServer(requestHandler)
+let sendResult = function (req, res, next) {
 
-server.listen(port, (err) => {
-  if (err) {
-    return console.log(`Server error`, err)
-  }
-  console.log(`Server listening on port ${port}\n`)
+  let status = res.locals.code || 200
+
+  res.status(status).json({
+    code:      status,
+    errors:    res.locals.errors    || [],
+    message:   res.locals.message   || null,
+    data:      res.locals.data      || null,
+    endpoint:  res.locals.endpoint  || req.url
+  })
+
+}
+
+app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*')
+  next()
+})
+
+app.route('/nodes')
+.get(function (req, res, next) {
+  res.locals.data = data.nodes
+  next()
+}, sendResult)
+
+app.use('/', router)
+
+// Handle 404 not found result
+app.all('*', function (req, res, next) {
+  res.locals.code = 404
+  res.locals.errors = ['404 Not Found']
+  next()
+}, sendResult)
+
+app.listen(port, function () {
+  console.log(`Server started on port ${port}`)
 })
