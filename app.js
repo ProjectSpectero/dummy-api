3a610dcc6810bf4125602248affe04d9682a2f5f
Anatolie Diordita <contact@anatolie.me>
2017-10-24 01:26:35 -0400

    New router, got rid of switch statements, separated dummy data into separate file.

--- app.js
+++ app.js
@@ -8,82 +8,67 @@
 const http = require('http')
 const port = 3000
 
+const data = require('./dummy-data.js')
+
 const requestHandler = (request, response) => {
   
-  let result
-
-  let notFound = function () {
-    response.statusCode = 404
-    response.end(`404 not found`)
+  let result = {
+    code: 200,
+    errors: [],
+    message: null,
+    data: null,
+    endpoint: null
   }
-  
-  let notAllowed = function () {
+
+  let routes = {
+    '/nodes': {
+      GET: function () {
+        return {
+          nodes: data.nodes()
+        }
+      }
+    }
+  }
+
+  let errorNotFound = function (result) {
+    result.code = 404
+    result.errors.push(`404 Not Found`)
+    response.statusCode = 404
+    return response.end(JSON.stringify(result))
+  }
+
+  let errorMethodNotAllowed = function (result) {
+    result.code = 405
+    result.errors.push(`404 Method Not Allowed`)
     response.statusCode = 405
-    response.end(`405 not allowed`)
+    return response.end(JSON.stringify(result))
+  }
+
+  console.log(`${request.method}  ${request.url}`)
+
+  // Check if URL has route associated to it
+  if ( routes[request.url] === undefined ) {
+    return errorNotFound(result)
+  }
+
+  // Instead of having to declare GET method, can just directly feed
+  // a function into the route if only GET method req'd.
+  if ( typeof routes[request.url] === 'function' ) {
+    result.data = routes[request.url]()
+  }
+
+  // Check if route method exists
+  else if ( routes[request.url][request.method] === undefined ) {
+    return errorMethodNotAllowed(result)
+  }
+
+  else {
+    result.data = routes[request.url][request.method]()
   }
 
   response.setHeader(`Content-Type`, `application/json`)
   response.setHeader(`Access-Control-Allow-Origin`, `*`)
-
-  switch ( request.url ) {
-    case '/nodes':
-      switch ( request.method ) {
-        case 'GET':
-          result = JSON.stringify({
-            result: {
-              nodes: [
-                {
-                  id: 1,
-                  title: 'dxbpi1',
-                  ip: '94.202.105.172',
-                  status: 'offline',
-                  active: true
-                },
-                {
-                  id: 2,
-                  title: 'seapi1',
-                  ip: '50.47.111.48',
-                  status: 'online',
-                  active: true
-                },
-                {
-                  id: 3,
-                  title: 'seapi2',
-                  ip: '67.170.105.255',
-                  status: 'online',
-                  active: true
-                },
-                {
-                  id: 4,
-                  title: 'cadpi1',
-                  ip: '74.334.224.48',
-                  status: 'online',
-                  active: true
-                },
-                {
-                  id: 5,
-                  title: 'dxbpi2',
-                  ip: '94.202.105.173',
-                  status: 'offline',
-                  active: true
-                },
-              ]
-            }
-          })
-        break
-        default: notAllowed()
-      }
-    break
-    default: notFound()
-  }
-
-  if ( result ) {
-    // console.log(`Serving ${request.url}`)
-    response.end(result)
-  }
-  else {
-    notFound()
-  }
+  response.end(JSON.stringify(result))
   
 }
 
@@ -93,5 +78,5 @@ server.listen(port, (err) => {
   if (err) {
     return console.log(`Server error`, err)
   }
-  console.log(`Server listening on port ${port}`)
+  console.log(`Server listening on port ${port}\n`)
 })
